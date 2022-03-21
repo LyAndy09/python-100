d5e43abd7ad8386cc1f2380fb61ff0f2452c6f89
LyAndy09 <96896728+LyAndy09@users.noreply.github.com>
2022-03-21 11:24:22 -0700

    first

--- Day 2/TimeLeft.py
+++ Day 2/TimeLeft.py
@@ -1,2 +1,13 @@
 #Code will display how much time we have left from an arbitrarily chosen 90 years.
 
+#From Assignment
+age = input("what is your current age?")
+
+#My code
+days = 90 * 365 - int(age) * 365
+
+weeks = 90 * 52 - int(age) * 52
+
+months = 90 * 12 - int(age) * 12
+
+print(f"You have {days} days , {weeks} weeks, {months} months left.")
