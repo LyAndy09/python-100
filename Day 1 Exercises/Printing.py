5d6a3c96314a368e0142bdcef79909882a4b5b24
LyAndy09 <96896728+LyAndy09@users.noreply.github.com>
2022-03-21 10:55:12 -0700

    Create Printing.py

---
+++ Day 1 Exercises/Printing.py
@@ -0,0 +1,3 @@
+#Understanding the input function. 
+#Code will ask the user to input a name, then it counts the amount of characters within the input and outputs the total character count.
+print(len(input("What is your name?")))
