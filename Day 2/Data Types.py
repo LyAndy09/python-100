abd83fa6a0535a2523107907c77572c859a89662
LyAndy09 <96896728+LyAndy09@users.noreply.github.com>
2022-03-21 11:17:14 -0700

    first

---
+++ Day 2/Data Types.py
@@ -0,0 +1,6 @@
+#Program is intended to add the digits in a two digit number. If input is 27, then 2+7 is the output
+
+two_digit_number = input("Type a two digit number")
+
+addition = int(two_digit_number[0]) + int(two_digit_number[1])
+print(addition)
